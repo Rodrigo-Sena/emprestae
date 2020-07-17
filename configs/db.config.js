a376c0fc0ca0d308f5d8527487b9f0b7562fbed7
Rodrigo-Sena <sena.rodrigo@outlook.com>
2020-07-17 19:29:07 +0100

    heroku

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+
 mongoose
-  .connect('mongodb://localhost/emprestae', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
